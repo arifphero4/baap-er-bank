f5f623b85fcbc393bcd1a3a1e5421eff43ad48d5
arifphero <ahammadarif232@gmail.com>
2021-08-19 02:43:19 +0600

    index commit

---
+++ login.js
@@ -0,0 +1,17 @@
+document.getElementById('login-submit').addEventListener('click', function () {
+    // get user email
+    const emailField = document.getElementById('user-email');
+    const userEmail = emailField.value; 
+    // get user password
+    const passwordField = document.getElementById('user-password');
+    const userPassword = passwordField.value;
+    // check email and password
+    if ( userEmail == 'arif@hasan.com' && userPassword == '12345'){
+        window.location.href = 'banking.html';
+    }
+    });
+
+
+
+
+
